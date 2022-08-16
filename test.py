2851a876212fdbf88ea1cb45f6fd30a028e0a0e4
Yunpyo An <anyunpyo@unist.ac.kr>
2022-08-16 09:59:43 +0000

    remove test

--- test.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-sys_details = tf.sysconfig.get_build_info()
-cuda_version = sys_details["cuda_version"]
-print(cuda_version)
-cudnn_version = sys_details["cudnn_version"]  
-print(cudnn_version)
-cuda_compute_capabilities = sys_details["cuda_compute_capabilities"]  
-print(cuda_compute_capabilities)
