fix similarity computation

utils/epochs.py
@@ -136,12 +136,12 @@ def run_round(model,
             total_sim += similarity
             similar_list.append(similarity)
 
-        print("similar: ", similar_list)
         pmf = list(map(lambda item: item/total_sim, similar_list))
     else:
         # Select as random
         pmf = random_pmf(args.num_clients)
 
+    print("pmf: ", pmf)
     # Sampling client
     selected_clients = np.random.choice(args.num_clients, args.active_selection, replace=False, p=pmf)
 

utils/toolkit.py
@@ -25,12 +25,14 @@ def get_last_param(model):
 def get_similarity(args, vec1, vec2):
     assert vec1.shape == vec2.shape
 
-    vec2 = vec2 / torch.norm(vec2) # normalize vector
+    vec1 = torch.flatten(vec1)
+    vec2 = torch.flatten(vec2)
+    vec2 = vec2 / torch.norm(vec2)
 
     if args.similarity_measure == 'distance':
         ret = (vec1 - vec2).pow(2).sum().sqrt().item()
     elif args.similarity_measure == 'similar':
         cos = nn.CosineSimilarity(dim=0)
-        ret = cos(vec1, vec2)
+        ret = cos(vec1, vec2).item()
 
     return ret